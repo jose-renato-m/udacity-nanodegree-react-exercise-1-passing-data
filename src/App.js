fd6e09acda215f18990c3f449a5245a48653a9b4
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-10-18 20:02:47 +0000

    feat: Import component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import logo from './logo.svg';
+import ListUsers from './ListUsers.js';
 
 /*
 Use React and the data below to display a list of users alongside their favorite movies.
