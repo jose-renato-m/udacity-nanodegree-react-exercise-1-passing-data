f4b05ac96228dfcf1f301567c58857d1b063450c
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-10-18 21:53:12 +0000

    feat: Map array profiles

--- src/ListProfiles.js
+++ src/ListProfiles.js
@@ -4,7 +4,11 @@ class ListProfiles extends Component {
   render() {
     return (
       <ol className='profile-list'>
-      
+      {this.props.profiles.map((profile) => (
+        <li key={profile.id}>
+         {profile.userID}
+        </li>
+      ))}
       </ol>
     )
   }
