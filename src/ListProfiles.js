190aa79e5790d709031b2c6752ed76b02b88d449
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-10-21 15:40:56 +0000

    feat: Display list of users and favorite movies

--- src/ListProfiles.js
+++ src/ListProfiles.js
@@ -5,8 +5,10 @@ class ListProfiles extends Component {
     return (
       <ol className='profile-list'>
       {this.props.profiles.map((profile) => (
-        <li key={profile.id}>
-         {profile.userID}
+        <li key={profile.id} className='profile-list-item'>
+          <div className='user-favorite-movie'>
+           {`${id.users.name}'s favorite movie is ${favoriteMovieID.movies.name}.`}
+          </div>
         </li>
       ))}
       </ol>
